4c2d03c69f5ce11010b5fd6e8daf58f1ff9342db
James Ashby <60621690+james-ashby@users.noreply.github.com>
2020-10-06 16:57:56 +0100

    Removed sensitive information from Config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'e8qsG1g3nEDPTrX4oXt8c7V5Z',
-    consumer_secret:      'YJsZUMT0STX2S9grV4ty5iiUZqZNBaQewRsMnncOKqlqq6WZXP',
-    access_token:         '1311999359668625409-xzVly89ajKSVu8e6r9kC3eXgVEsPBd',
-    access_token_secret:  'W2ac283RCm5pDNPuLnJLXM7IgQX11LWttaBrmm0sXJWFQ',
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
 }
